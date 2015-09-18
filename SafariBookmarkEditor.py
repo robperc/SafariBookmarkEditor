d21e38d585106efe6d5ccb50f9c368627661e0d8
Robert Percival <robert@mactechs.com>
2015-09-18 11:37:37 -0700

    Implemented command line argument functionality. Implemented bookmark removal. Added boolean return value to readPlist to notify if plist had to be converted from binary form.

--- SafariBookmarkEditor.py
+++ SafariBookmarkEditor.py
@@ -2,7 +2,7 @@
 # Copyright MacTechs, 2014. All rights reserved.
 # Intended for use with Mac OS 10.10.4 
 
-
+import argparse
 import os
 import plistlib
 import subprocess
@@ -21,34 +21,68 @@ def getPlist(plist):
 	print "Plist not found in user home directory."
 	return None
 
-# Returns dict containing information read from plist file.
+# Returns dict containing information read from plist file and
+# a boolean value stating whether the plist had to be converted.
 # Converts plist to xml1 form before reading if it is a binary plist.
 def readPlist(plist_path):
+	converted = False
 	print "Reading %s into dict" % (plist_path)
 	try:
 		pl = plistlib.readPlist(plist_path)
 	except:
 		print "Plist appears to be in binary form, converting to xml"
+		converted = True
 		subprocess.call(['plutil', '-convert', 'xml1', plist_path])
 		pl = plistlib.readPlist(plist_path)
-	return pl
+	return pl, converted
 
 # Adds a bookmark to the plist dictionary.
-def addBookmark(plist, name, url):
+# If title of bookmark to be added is the same as a 
+# preexisting bookmark the bookmark is skipped.
+def addBookmark(plist, title, url):
+	print "Checking for preexisting bookmarks title %s" % (title)
+	if findTitle(plist, title):
+		print "Found preexisting bookmark with title %s, skipping" % (title)
+		return None
+	print "No preexisting bookmarks with title %s" % (title)
+	print "Adding bookmark to %s with title %s" % (url, title)
 	uri_dict = dict(
-		title=name
+		title=title
 	)
 	bookmark = dict(
 		WebBookmarkType='WebBookmarkTypeLeaf',
-		WebBookmarkUUID=str(uuid.uuid5(uuid.NAMESPACE_DNS, name)),
+		WebBookmarkUUID=str(uuid.uuid5(uuid.NAMESPACE_DNS, title)),
 		URLString=url,
 		URIDictionary=uri_dict,
 	)
 	plist['Children'][1]['Children'].append(bookmark)
 
 # Removes a bookmark from the plist dictionary.
-def removeBookmark(plist):
-	pass
+def removeBookmark(plist, title):
+	print "Removing bookmark with title %s" % (title)
+	if findTitle(plist, title):
+		print "Bookmark found and removed"
+		return None
+	print "Could not find bookmark with title %s, skipping" % (title)
+
+# Searches for bookmark with provided title
+# Returns True if found, False otherwise
+def findTitle(plist, title):
+	for bookmark in plist['Children'][1]['Children']:
+		found_title = bookmark['URIDictionary']['title']
+		if title == found_title:
+			return True
+	return False
+
+# Removes all bookmarks from the plist dictionary
+def removeAll(plist):
+	print "Removing all bookmarks"
+	# Remove bookmarks in reveresed order to avoid shifting issues
+	for bookmark in reversed(plist['Children'][1]['Children']):
+		title = bookmark['URIDictionary']['title']
+		print "Removing bookmark w/ title %s" % (title)
+		plist['Children'][1]['Children'].remove(bookmark)
+
 
 # Writes plist to specified path.
 # Converts to binary form after writing if binary=True.
@@ -61,15 +95,32 @@ def writePlist(plist, plist_path, binary=False):
 	print "Done"
 
 def main():
-	to_add = {"Nfl.com": "http://www.nfl.com", "reddit": "http://www.reddit.com"}
+	parser = argparse.ArgumentParser(
+		description='Command line tool for adding and removing Safari bookmarks in the context of the currently logged in user',
+	)
+	parser.add_argument('--add', metavar='title|url', type=str, nargs='+', 
+		help='double-colon seperated title and url of bookmark(s) to add in IE: --add MyWebsite::http://www.mywebsite.com MyOtherWebsite::http://www.myotherwebsite.com',
+	)
+	parser.add_argument('--remove', metavar='title', type=str, nargs='+', 
+		help='title(s) of bookmark(s) to remove IE: --remove MyWebsite MyOtherWebsite',
+	)
+	parser.add_argument('--removeall', action='store_true', help='remove all current bookmarks')
+	args = parser.parse_args()
+	#to_add = {"Nfl.com": "http://www.nfl.com", "reddit": "http://www.reddit.com"}
 	plist_path = getPlist('Bookmarks.plist')
 	if plist_path is None:
 		sys.exit(1)
-	plist = readPlist(plist_path)
-	for name, url in to_add.iteritems():
-		addBookmark(plist, name, url)
-	print plist
-	writePlist(plist, plist_path, binary=True)
+	plist, converted = readPlist(plist_path)
+	if args.removeall:
+		removeAll(plist)
+	if args.remove:
+		for title in args.remove:
+			removeBookmark(plist, title)
+	if args.add:
+		for bookmark in args.add:
+			title, url = bookmark.split('::')
+			addBookmark(plist, title, url)
+	writePlist(plist, plist_path, binary=converted)
 
 
 if __name__ == "__main__":
