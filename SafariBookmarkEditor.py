c91cb8ccf47451766bab5cd9d091443a45c86849
Robert Percival <robert@mactechs.com>
2015-09-18 12:23:57 -0700

    Fixed removeBookmark so that it actually removed a found bookmark. Added optional argument to findTitle to delete a found bookmark if specified. Modified getPlist to display a warning and exit if more than one plist of the same provided filename is found in the user Library.

--- SafariBookmarkEditor.py
+++ SafariBookmarkEditor.py
@@ -9,18 +9,26 @@ import subprocess
 import sys
 import uuid
 
+# Searches user's library for specified plist.
 # Returns path to plist if it exists, None otherwise.
 def getPlist(plist):
-	user_home = os.path.expanduser('~')
-	print "searching %s for %s" % (user_home, plist)
+	user_home = os.path.join(os.path.expanduser('~'), 'Library')
+	print "searching %s for %s." % (user_home, plist)
+	paths = []
 	for root, dirs, files in os.walk(user_home):
+		# Ignore hidden directories and paths
 		dirs[:] = [d for d in dirs if not d[0] == '.']
 		files = [f for f in files if not f[0] == '.']
 		if plist in files:
 			path = os.path.join(root, plist)
-			print "found: %s" % path
-			return path
-	print "Plist not found in user home directory."
+			print "found: %s." % path
+			paths.append(path)
+	if len(paths) == 1:
+		return paths[0]
+	elif len(paths) > 1:
+		print "More than one %s found, unsafe to continue." % (plist)
+	else:
+		print "%s not found in user Library." % (plist)
 	return None
 
 # Returns dict containing information read from plist file and
@@ -28,11 +36,11 @@ def getPlist(plist):
 # Converts plist to xml1 form before reading if it is a binary plist.
 def readPlist(plist_path):
 	converted = False
-	print "Reading %s into dict" % (plist_path)
+	print "Reading %s into dict." % (plist_path)
 	try:
 		pl = plistlib.readPlist(plist_path)
 	except:
-		print "Plist appears to be in binary form, converting to xml"
+		print "Plist appears to be in binary form, converting to xml."
 		converted = True
 		subprocess.call(['plutil', '-convert', 'xml1', plist_path])
 		pl = plistlib.readPlist(plist_path)
@@ -42,12 +50,12 @@ def readPlist(plist_path):
 # If title of bookmark to be added is the same as a 
 # preexisting bookmark the bookmark is skipped.
 def addBookmark(plist, title, url):
-	print "Checking for preexisting bookmarks title %s" % (title)
+	print "Checking for preexisting bookmarks title %s." % (title)
 	if findTitle(plist, title):
-		print "Found preexisting bookmark with title %s, skipping" % (title)
-		return None
-	print "No preexisting bookmarks with title %s" % (title)
-	print "Adding bookmark to %s with title %s" % (url, title)
+		print "Found preexisting bookmark with title %s, skipping." % (title)
+		return
+	print "No preexisting bookmarks with title %s." % (title)
+	print "Adding bookmark to %s with title %s." % (url, title)
 	uri_dict = dict(
 		title=title
 	)
@@ -62,18 +70,21 @@ def addBookmark(plist, title, url):
 # Removes a bookmark from the plist dictionary.
 # If the bookmark doesn't exist then skips
 def removeBookmark(plist, title):
-	print "Removing bookmark with title %s" % (title)
-	if findTitle(plist, title):
-		print "Bookmark found and removed"
-		return None
-	print "Could not find bookmark with title %s, skipping" % (title)
+	print "Removing bookmark with title %s." % (title)
+	if findTitle(plist, title, remove=True):
+		print "Bookmark found and removed."
+		return
+	print "Could not find bookmark with title %s, skipping." % (title)
 
 # Searches plist for bookmark with provided title
 # Returns True if found, False otherwise
-def findTitle(plist, title):
+# If remove is True removes the bookmark if found
+def findTitle(plist, title, remove=False):
 	for bookmark in plist['Children'][1]['Children']:
 		found_title = bookmark['URIDictionary']['title']
 		if title == found_title:
+			if remove:
+				plist['Children'][1]['Children'].remove(bookmark)
 			return True
 	return False
 
@@ -83,14 +94,14 @@ def removeAll(plist):
 	# Remove bookmarks in reveresed order to avoid shifting issues
 	for bookmark in reversed(plist['Children'][1]['Children']):
 		title = bookmark['URIDictionary']['title']
-		print "Removing bookmark w/ title %s" % (title)
+		print "Removing bookmark w/ title %s." % (title)
 		plist['Children'][1]['Children'].remove(bookmark)
 
 
 # Writes plist to specified path.
 # Converts to binary form after writing if binary=True.
 def writePlist(plist, plist_path, binary=False):
-	print "Writing modified plist to %s" % (plist_path)
+	print "Writing modified plist to %s." % (plist_path)
 	plistlib.writePlist(plist, plist_path)
 	if binary:
 		print "Converting %s to binary." % (plist_path)
@@ -99,7 +110,7 @@ def writePlist(plist, plist_path, binary=False):
 
 def main():
 	parser = argparse.ArgumentParser(
-		description='Command line tool for adding and removing Safari bookmarks in the context of the currently logged in user',
+		description='Command line tool for adding and removing Safari bookmarks in the context of the currently logged in user.',
 	)
 	parser.add_argument('--add', metavar='title|url', type=str, nargs='+', 
 		help='double-colon seperated title and url of bookmark(s) to add in IE: --add MyWebsite::http://www.mywebsite.com MyOtherWebsite::http://www.myotherwebsite.com',
